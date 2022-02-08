c0e607d140e5b9f709461fc3e9fa3dc04ff30723
wenhui.bai <strongant.bai@baozun.com>
2022-02-08 19:07:06 +0800

    实现Consul 到Conusl 的同步

---
+++ nacossync-worker/src/main/java/com/alibaba/nacossync/extension/impl/ConsulSyncToConsulServiceImpl.java
@@ -0,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.alibaba.nacossync.extension.impl;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacossync.cache.SkyWalkerCacheServices;
+import com.alibaba.nacossync.constant.ClusterTypeEnum;
+import com.alibaba.nacossync.constant.MetricsStatisticsType;
+import com.alibaba.nacossync.constant.SkyWalkerConstants;
+import com.alibaba.nacossync.extension.SyncService;
+import com.alibaba.nacossync.extension.annotation.NacosSyncService;
+import com.alibaba.nacossync.extension.event.SpecialSyncEventBus;
+import com.alibaba.nacossync.extension.holder.ConsulServerHolder;
+import com.alibaba.nacossync.monitor.MetricsManager;
+import com.alibaba.nacossync.pojo.model.TaskDO;
+import com.alibaba.nacossync.util.ConsulUtils;
+import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.QueryParams;
+import com.ecwid.consul.v1.Response;
+import com.ecwid.consul.v1.agent.model.NewService;
+import com.ecwid.consul.v1.health.model.HealthService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.*;
+
+/**
+ * 支持 Consul 同步 Consul
+ */
+@Slf4j
+@NacosSyncService(sourceCluster = ClusterTypeEnum.CONSUL, destinationCluster = ClusterTypeEnum.CONSUL)
+public class ConsulSyncToConsulServiceImpl implements SyncService {
+
+    @Autowired
+    private MetricsManager metricsManager;
+
+    private final ConsulServerHolder consulServerHolder;
+    private final SkyWalkerCacheServices skyWalkerCacheServices;
+
+    private final ConsulServerHolder destConsulServerHolder;
+
+    private final SpecialSyncEventBus specialSyncEventBus;
+
+    @Autowired
+    public ConsulSyncToConsulServiceImpl(ConsulServerHolder consulServerHolder,
+                                         SkyWalkerCacheServices skyWalkerCacheServices,
+                                         ConsulServerHolder destConsulServerHolder,
+                                         SpecialSyncEventBus specialSyncEventBus) {
+        this.consulServerHolder = consulServerHolder;
+        this.skyWalkerCacheServices = skyWalkerCacheServices;
+        this.destConsulServerHolder = destConsulServerHolder;
+        this.specialSyncEventBus = specialSyncEventBus;
+    }
+
+    @Override
+    public boolean delete(TaskDO taskDO) {
+
+        try {
+            specialSyncEventBus.unsubscribe(taskDO);
+
+            ConsulClient destConsulClient = destConsulServerHolder.get(taskDO.getDestClusterId());
+            Response<List<HealthService>> allInstances = destConsulClient.getHealthServices(taskDO.getServiceName(), true , QueryParams.DEFAULT);
+            for (HealthService instance : allInstances.getValue()) {
+                if (needDelete(instance.getService().getMeta(), taskDO)) {
+                    destConsulClient.agentServiceDeregister(taskDO.getServiceName(),null);
+                }
+            }
+
+        } catch (Exception e) {
+            log.error("delete task from consul to consul was failed, taskId:{}", taskDO.getTaskId(), e);
+            metricsManager.recordError(MetricsStatisticsType.DELETE_ERROR);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean sync(TaskDO taskDO) {
+        try {
+            ConsulClient consulClient = consulServerHolder.get(taskDO.getSourceClusterId());
+            ConsulClient destConsulClient = destConsulServerHolder.get(taskDO.getDestClusterId());
+           List<HealthService> healthServiceList = consulClient.getHealthServices(taskDO.getServiceName(), true, QueryParams.DEFAULT).getValue();
+            Set<String> instanceKeys = new HashSet<>();
+            overrideAllInstance(taskDO, destConsulClient, healthServiceList, instanceKeys);
+            cleanAllOldInstance(taskDO, destConsulClient, instanceKeys);
+            specialSyncEventBus.subscribe(taskDO, this::sync);
+        } catch (Exception e) {
+            log.error("Sync task from consul to consul was failed, taskId:{}", taskDO.getTaskId(), e);
+            metricsManager.recordError(MetricsStatisticsType.SYNC_ERROR);
+            return false;
+        }
+        return true;
+    }
+
+    private void cleanAllOldInstance(TaskDO taskDO, ConsulClient destNamingService, Set<String> instanceKeys) {
+        List<HealthService> allInstances = destNamingService.getHealthServices(taskDO.getServiceName(),true,QueryParams.DEFAULT).getValue();
+        for (HealthService instance : allInstances) {
+            if (needDelete(instance.getService().getMeta(), taskDO)
+                && !instanceKeys.contains(composeInstanceKey(instance.getService().getAddress(), instance.getService().getPort()))) {
+
+                destNamingService.agentServiceDeregister(instance.getService().getId(),null);
+            }
+        }
+    }
+
+    private void overrideAllInstance(TaskDO taskDO, ConsulClient destNamingService,
+        List<HealthService> healthServiceList, Set<String> instanceKeys) {
+        for (HealthService healthService : healthServiceList) {
+            if (needSync(ConsulUtils.transferMetadata(healthService.getService().getTags()))) {
+                destNamingService.agentServiceRegister(buildSyncInstance(healthService, taskDO));
+                instanceKeys.add(composeInstanceKey(healthService.getService().getAddress(),
+                    healthService.getService().getPort()));
+            }
+        }
+    }
+
+    private NewService buildSyncInstance(HealthService instance, TaskDO taskDO) {
+        NewService temp = new NewService();
+        temp.setAddress(instance.getService().getAddress());
+        temp.setPort(instance.getService().getPort());
+        Map<String, String> metaData = new HashMap<>(ConsulUtils.transferMetadata(instance.getService().getTags()));
+        metaData.put(SkyWalkerConstants.DEST_CLUSTERID_KEY, taskDO.getDestClusterId());
+        metaData.put(SkyWalkerConstants.SYNC_SOURCE_KEY,
+            skyWalkerCacheServices.getClusterType(taskDO.getSourceClusterId()).getCode());
+        metaData.put(SkyWalkerConstants.SOURCE_CLUSTERID_KEY, taskDO.getSourceClusterId());
+        temp.setMeta(metaData);
+        return temp;
+    }
+
+    private String composeInstanceKey(String ip, int port) {
+        return ip + ":" + port;
+    }
+
+}
